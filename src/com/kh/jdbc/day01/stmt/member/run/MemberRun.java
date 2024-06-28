d3667e3865a6f468020b8970016c0f02f76b81a3
yoonneeeee <imyk.athena@gmail.com>
2024-06-28 17:35:18 +0900

    [ADD] JDBC 1일차 - 회원관리 프로그램
    1. Statement 사용
    2. 회원가입, 회원 전체조회, 회원 검색 기능완성
    3. 자주 사용되는 값 final상수 처리

---
+++ src/com/kh/jdbc/day01/stmt/member/run/MemberRun.java
@@ -0,0 +1,12 @@
+package com.kh.jdbc.day01.stmt.member.run;
+
+import com.kh.jdbc.day01.stmt.member.view.MemberView;
+
+public class MemberRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MemberView view = new MemberView();
+		view.startProgram();
+	}
+}
